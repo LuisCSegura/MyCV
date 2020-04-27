83ba37f4724570de4f9fba365dcb2e1b47c6f8e9
Luis C. Segura <luiscarlossegura.b@gmail.com>
2020-04-27 01:47:59 -0600

    Middlewares

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -7,6 +7,10 @@ use App\Contribution;
 
 class ContributionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -7,6 +7,10 @@ use App\Experience;
 
 class ExperienceController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
