1f9af125187302b10a21de81306cfc4c2800ee59
Luis C. Segura <luiscarlossegura.b@gmail.com>
2020-04-28 13:40:28 -0600

    send mails

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App;
+use Illuminate\Support\Facades\Mail;
 
 class PDFController extends Controller
 {
@@ -26,19 +27,28 @@ class PDFController extends Controller
 
         return $pdf->download('MyCV.pdf');
     }
+    public function sendMail($style)
+    {
 
+        $id = auth()->user()->id;
+        $email = auth()->user()->email;
+        $pdf = app('dompdf.wrapper');
+        $profile = App\Profile::where('user_id', $id)->get();
+        $contributions = App\Contribution::where('user_id', $id)->get();
+        $educations = App\Education::where('user_id', $id)->get();
+        $experiences = App\Experience::where('user_id', $id)->get();
+        $hobbies = App\Hobbie::where('user_id', $id)->get();
+        $knowledges = App\Knowledge::where('user_id', $id)->get();
+        $projects = App\Project::where('user_id', $id)->get();
+        $skills = App\Skill::where('user_id', $id)->get();
+        $pdf->loadView('templates.pdfCV', compact('style', 'profile', 'contributions', 'educations', 'experiences', 'hobbies', 'knowledges', 'projects', 'skills'));
 
-    // $id = auth()->user()->id;
-    //     $profile = App\Profile::where('user_id', $id)->get();
-    //     $contributions = App\Contribution::where('user_id', $id)->get();
-    //     $educations = App\Education::where('user_id', $id)->get();
-    //     $experiences = App\Experience::where('user_id', $id)->get();
-    //     $hobbies = App\Hobbie::where('user_id', $id)->get();
-    //     $knowledges = App\Knowledge::where('user_id', $id)->get();
-    //     $projects = App\Project::where('user_id', $id)->get();
-    //     $skills = App\Skill::where('user_id', $id)->get();
-    //     $pdf = app('dompdf.wrapper');
-    //     $pdf->loadView('templates.CV', compact('style', 'profile', 'contributions', 'educations', 'experiences', 'hobbies', 'knowledges', 'projects', 'skills'));
-
-    //     return $pdf->download('archivo.pdf');
+        Mail::send('templates.mail', compact('style'), function ($mail) use ($pdf, $email) {
+            $mail->from('lsegurab@est.utn.ac.cr');
+            $mail->to($email);
+            $mail->subject('Your Resume');
+            $mail->attachData($pdf->output(), 'test.pdf');
+        });
+        return view('templates.CV', compact('style', 'profile', 'contributions', 'educations', 'experiences', 'hobbies', 'knowledges', 'projects', 'skills'));
+    }
 }
