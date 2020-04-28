6ed748cb5b7b9bf2826a0fe2c9a430a002bc6686
Luis C. Segura <luiscarlossegura.b@gmail.com>
2020-04-27 23:03:35 -0600

    download pdf

--- resources/views/templates/CV.blade.php
+++ resources/views/templates/CV.blade.php
@@ -11,10 +11,16 @@
 
 <body>
 
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
+    <script type="text/javascript" src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
 
 
     @section('content')
-    <div class="container">
+    <form class="export-bar" action="">
+        <input type="button" class="export-bar-button" onclick="CreatePDFfromHTML()" value="Download as PDF">
+        <input type="submit" class="export-bar-button" Value>
+    </form>
+    <div class="content-section">
         <link rel="stylesheet" href="{{ asset('css/'.$style.'.css') }}">
         <div id="doc2" class="yui-t7">
             <div id="inner">
@@ -220,6 +226,30 @@
         </div>
     </div>
     @endsection
+    <script>
+        function CreatePDFfromHTML() {
+            var HTML_Width = $(".content-section").width();
+            var HTML_Height = $(".content-section").height();
+            var top_left_margin = 15;
+            var PDF_Width = HTML_Width + (top_left_margin * 2);
+            var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+            var canvas_image_width = HTML_Width;
+            var canvas_image_height = HTML_Height;
+
+            var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+
+            html2canvas($(".content-section")[0]).then(function(canvas) {
+                var imgData = canvas.toDataURL("image/jpeg", 1.0);
+                var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+                pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+                for (var i = 1; i <= totalPDFPages; i++) {
+                    pdf.addPage(PDF_Width, PDF_Height);
+                    pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+                }
+                pdf.save("YourResume.pdf");
+            });
+        }
+    </script>
 </body>
 
 </html>
